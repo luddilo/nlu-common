86a38958aea726a2ef833b10bbeaa52022b1aa99
luddilo <ludvig@furhatrobotics.com>
2018-11-29 16:29:24 +0100

    initial test

---
+++ src/main/kotlin/furhatos/nlu/common/Ludvig.kt
@@ -0,0 +1,10 @@
+package furhatos.nlu.common
+
+import furhatos.nlu.Intent
+import furhatos.util.Language
+
+class Ludvig : Intent() {
+    override fun getExamples(lang: Language): List<String> {
+        return listOf("ludvig", "ludwig")
+    }
+}
