8cb90f91fa71cac39815204ba46d42fba372f5b5
Ludvig Linse <ludvig.linse@gmail.com>
2018-11-29 16:56:29 +0100

    Update Ludvig.kt
    Added an example

--- src/main/kotlin/furhatos/nlu/common/Ludvig.kt
+++ src/main/kotlin/furhatos/nlu/common/Ludvig.kt
@@ -5,6 +5,6 @@ import furhatos.util.Language
 
 class Ludvig : Intent() {
     override fun getExamples(lang: Language): List<String> {
-        return listOf("ludvig", "ludwig")
+        return listOf("ludvig", "ludwig", "mr ludvig")
     }
 }
